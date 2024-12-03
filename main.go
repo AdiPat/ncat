d57d11c45fa3676ed925596b3b68ef133fab19e1
Aditya Patange <contact.adityapatange@gmail.com>
2024-12-03 19:52:35 +0530

    main: add wrapper Ncat()

--- main.go
+++ main.go
@@ -6,6 +6,11 @@ import (
 	core "github.com/AdiPat/ncat/src"
 )
 
+func Ncat(options core.NcatOptions) core.NcatResult {
+	ncatClient := core.NcatClient{}
+	return ncatClient.Ncat(options)
+}
+
 func main() {
 	ncatClient := core.NcatClient{}
 	cli := core.NewCli(ncatClient)
